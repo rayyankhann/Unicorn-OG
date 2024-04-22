6961b387eaef367279178bc6deb0967e558addfd
trippie <109394821+rayyankhann@users.noreply.github.com>
2024-04-21 20:20:52 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId:         '',         // e.g. us-east-2_uXboG5pAb
-        userPoolClientId:   '',         // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region:             ''          // e.g. us-east-2
+        userPoolId:         'us-west-2_XmlD3q8zS',         // e.g. us-east-2_uXboG5pAb
+        userPoolClientId:   '1l1j4utonon1v6fs8dnjb23vnv',         // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region:             'us-west-2'          // e.g. us-east-2
     },
     api: {
         invokeUrl:          ''          // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
